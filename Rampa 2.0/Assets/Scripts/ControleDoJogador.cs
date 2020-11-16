418857d4d96bebb8fe216fad39d06279d38c0433
napaula3x <a231249@dac.unicamp.br>
2020-11-15 22:45:48 -0300

    Rampa 2.0

---
+++ Rampa 2.0/Assets/Scripts/ControleDoJogador.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class ControleDoJogador : MonoBehaviour
+{
+    public float velocidade = 2;
+    Vector3 direção;
+
+    //cor 0 = base
+    //cor 1 = verde
+    //cor 2 = vermelho
+    public Material[] Cores;
+
+    float tempo = 0;
+    public float tempoLimite = 3f;
+
+    Renderer renderizador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderizador = this.gameObject.GetComponent<Renderer>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        Movimentação();
+        ResetaCor();
+    }
+
+    private void ResetaCor()
+    {
+        if (tempo > tempoLimite)
+        {
+            renderizador.material = Cores[0];
+        }
+    }
+
+    private void Movimentação()
+    {
+        direção = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(direção * velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Colidi com " + other.gameObject.name);
+        if (other.GetComponent<Vermelho>() != null)
+        {
+            Morre();
+        }
+        else if (other.GetComponent<Verde>() != null)
+        {
+            if (renderizador.material = Cores[1])
+            {
+                Morre();
+            }
+            else
+            {
+                renderizador.material = Cores[1];
+                tempo = 0;
+            }
+        }
+    }
+
+    private void Morre()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Rampa 2.0/Assets/Scripts/audioAnimal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioAnimal : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (audioSource != null)
+            {
+                audioSource.PlayOneShot(clip);
+            }
+        }
+           
+    }
+}
